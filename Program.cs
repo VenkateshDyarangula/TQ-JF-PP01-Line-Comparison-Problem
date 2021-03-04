1c26d20cc3bb1c6f46548b3ecb68850b092659f7
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-04 14:53:00 +0530

    UC1_Line_Using_Cartesian_System (Branch) Added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 using System;
 
-namespace TQ_JF_PP01_Line_Comparison_Problem
+namespace UC1_Line_Using_Cartesian_System
 {
     class Program
     {
+        static double distance(int x1, int y1, int x2, int y2)
+        {
+            // Calculating Length
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                          Math.Pow(y2 - y1, 2) * 1.0);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enetr x1, y1 and x2, y2 Values");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Round(distance(x1, y1, x2, y2) * 100000.0) / 100000.0);
         }
+
     }
 }
